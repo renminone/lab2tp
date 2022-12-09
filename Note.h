370b18cb34b8b25ef7e6073f0c2d0a3a8baf0397
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-12-09 23:07:33 +0300

    Update Note.h

--- Note.h
+++ Note.h
@@ -5,20 +5,25 @@ using namespace std;
 
 class Note
 {
-	char *Surname;
-	char *Name;
-	int number;
-	int birthday[3];
+	char *surname;
+	char *name;
+	char* number;
+	short int* birthDate;
+	int months[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 public:
 
-	Note();
-	Note(char* s, char* n, int nu, int b[3]);
-	Note(const Note& obj);
-	~Note();
+	Note(void);
+	Note(char* s1, char* s2, char* s3, short day, short month, short year);
+	Note(const Note& toCopy);
+	~Note(void);
 
-	void set();
-	void get();
-
-	friend ostream& operator<<(ostream& stream, Note obj);
-	friend istream& operator>>(istream& stream, Note& obj);
+	void countDays(void);
+	unsigned days;
+	void change(void);
+	Note* get(void);
+	char* get(int propNum);
+	short getDate(int perNum);
+	void set(char* s1, char* s2, char* s3, short day, short month, short year);
+	friend ostream& operator<<(ostream& out, Note& per);
+	friend void operator>>(int perNum, Note& per);
 };
