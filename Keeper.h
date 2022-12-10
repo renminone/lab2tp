c13fe95d2a1cf012d6a04c11b3cb984152b386e9
Vlad Temnoholmov <101628587+renminone@users.noreply.github.com>
2022-12-11 01:38:25 +0300

    Update Keeper.h

--- Keeper.h
+++ Keeper.h
@@ -12,6 +12,7 @@ protected:
 	place* operator[](int i);
 public:
 	void add(Note* toInsert);
+	void arconv();
 	void addToEnd(Note* toInsert);
 	void insert(int pos, Note* toInsert);
 	void copy(int sou, int dest);
